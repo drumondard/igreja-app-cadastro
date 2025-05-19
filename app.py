e8a70ce2cc977c46f009e5a911ffca0740a60c77
Alexandre R Drumond <77971755+drumondard@users.noreply.github.com>
2025-05-19 20:34:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, Church Members!"
+
 if __name__ == '__main__':
-    init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
