1c82174ee0bb91c95a0cf9e4e4d89aaa3ee6afe6
Alexandre R Drumond <77971755+drumondard@users.noreply.github.com>
2025-05-19 22:30:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,17 +1,15 @@
-# models.py
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy()
-bcrypt = Bcrypt()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
-
-    def check_password(self, password):
-        return bcrypt.check_password_hash(self.password_hash, password)
+class Membro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    telefone = db.Column(db.String(20))
