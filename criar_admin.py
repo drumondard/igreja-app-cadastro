1dd2c697a238f76a20c983d067cbdc6e8c9784e7
Alexandre R Drumond <77971755+drumondard@users.noreply.github.com>
2025-05-19 22:30:25 -0300

    Update criar_admin.py

--- criar_admin.py
+++ criar_admin.py
@@ -1,14 +1,14 @@
-# criar_admin.py
-from app import app, db
-from models import User
+from models import db, User
+from werkzeug.security import generate_password_hash
+from app import app
 
 with app.app_context():
-    # Verifica se já existe um admin
+    db.create_all()
+
     if not User.query.filter_by(username='admin').first():
-        admin = User(username='admin')
-        admin.set_password('senha_segura123')
+        admin = User(username='admin', password=generate_password_hash('admin123'))
         db.session.add(admin)
         db.session.commit()
-        print("Usuário admin criado com sucesso.")
+        print("Usuário admin criado.")
     else:
         print("Usuário admin já existe.")
